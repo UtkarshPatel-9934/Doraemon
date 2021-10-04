3c01503f21de3dbf3e90aa3fad4f7478c1fa1298
Utkarsh_Patel 9934 <W0785541@myscc.ca>
2021-10-05 00:10:41 +0530

    Added - Complete the sql setup and initial migration and use your CRUD pages from assignment 2.

---
+++ Doraemon_Assignment2/ThemeDoraemon/Migrations/20211004174107_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ThemeDoraemon.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "character",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Character_Name = table.Column<string>(type: "TEXT", nullable: true),
+                    Favorite_Colour = table.Column<string>(type: "TEXT", nullable: true),
+                    Favorite_Food = table.Column<string>(type: "TEXT", nullable: true),
+                    Birth_Date = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_character", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "character");
+        }
+    }
+}
--- Doraemon_Assignment2/ThemeDoraemon/Startup.cs
+++ Doraemon_Assignment2/ThemeDoraemon/Startup.cs
@@ -14,26 +14,36 @@ namespace ThemeDoraemon
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
+            Environment = env;
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            if (Environment.IsDevelopment())
+            {
+                    services.AddDbContext<ThemeDoraemoncharacterContext>(options =>
+                    options.UseSqlite(Configuration.GetConnectionString("ThemeDoraemoncharacterContext")));
+            }
+            else{
+                    services.AddDbContext<ThemeDoraemoncharacterContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("characterContext")));
+            }
+
             services.AddRazorPages();
 
-            services.AddDbContext<ThemeDoraemoncharacterContext>(options =>
-                    options.UseSqlite(Configuration.GetConnectionString("ThemeDoraemoncharacterContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app)
         {
-            if (env.IsDevelopment())
+            if (Environment.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
@@ -49,7 +59,7 @@ namespace ThemeDoraemon
 
             app.UseRouting();
 
-            app.UseAuthorization();
+            // app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
